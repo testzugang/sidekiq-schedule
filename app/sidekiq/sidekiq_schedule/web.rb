d8dc6817525582d3c32bc325654f31228ea40c05
Meinhard Gredig <mg@flexcoding.de>
2016-03-28 22:49:09 +0200

    Require Sidekiq:Web before registering web extension

--- app/sidekiq/sidekiq_schedule/web.rb
+++ app/sidekiq/sidekiq_schedule/web.rb
@@ -2,10 +2,8 @@ require "sidekiq_schedule/web_extension"
 
 module Web
 
-  if defined?(Sidekiq::Web)
-    Sidekiq::Web.register SidekiqSchedule::WebExtension
-
-    Sidekiq::Web.tabs["Scheduled Jobs"] = "scheduled_jobs"
-  end
-
+  require 'sidekiq/web' unless defined?(Sidekiq::Web)
+  Sidekiq::Web.register SidekiqSchedule::WebExtension
+  Sidekiq::Web.tabs["Scheduled Jobs"] = "scheduled_jobs"
+  Sidekiq::Web.set :locales, Sidekiq::Web.locales << File.expand_path(File.dirname(__FILE__) + "/../../../config/locales")
 end
